fd7585c00b49496ea51c63362bbd8655d0af2e7a
faizan711 <faizanalam980@gmail.com>
2026-01-08 12:49:42 +0530

    basic nodejs http server without express, and routing and logging and query and JSON body parsing

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+import http from "http";
+import fs from "fs";
+import url from "url";
+
+const myServer = http.createServer((req, res) => {
+  // res.end("Hello, this is my basic http server!");
+
+  //ignore favicon requests as this is the first request browser makes when loading a page and is not needed to be entertained by our server
+  if (req.url === "/favicon.ico") {
+    res.end();
+    return;
+  }
+
+  //using Date function to give timestamp to our logs
+  const time = new Date();
+  const log = `${time.toLocaleTimeString()} ${req.url} ${req.method} ${req.socket.remoteAddress}\n`;
+
+  fs.appendFile("log.txt", log, (err) => {
+    if (!err) console.log("log added!");
+  });
+
+  const myUrl = url.parse(req.url, true);
+
+  if (req.method == "POST" || req.method == "PUT") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      try {
+        const parseBody = JSON.parse(body);
+        const myUrl = url.parse(req.url, true);
+        handleRouting(myUrl, parseBody, res);
+      } catch (err) {
+        res.statusCode = 400;
+        res.end("Invalid JSON body");
+      }
+    });
+  } else {
+    handleRouting(myUrl, null, res);
+  }
+
+  //routing is happening for this server in basic http server
+  function handleRouting(myUrl, body, res) {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("welcome to my server");
+        break;
+      case "/about":
+        const myName = myUrl.query.name || "Guest";
+        res.end(`this route is for about of my ${myName} server`);
+        break;
+      case "/contact":
+        res.end("this is contact end of my server");
+        break;
+      case "/test-body":
+        console.log("Received body:", body);
+        res.end(`Received body: ${JSON.stringify(body)}`);
+        break;
+      default:
+        res.statusCode = 404;
+        res.end("404 - not found");
+    }
+  }
+});
+
+myServer.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
